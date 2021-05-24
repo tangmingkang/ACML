5b6b9c3d81961062c107ec0ef1f8722b6fa80ac3
TangMingkang <1183339199@qq.com>
2021-05-24 21:46:04 +0800

    修改READEME

--- resize.py
+++ resize.py
@@ -1,7 +1,8 @@
 from PIL import Image
 
 import os  
-  
+img_path='/home/data/ISIC/ISIC2020/jpeg/test'
+save_path='/home/data/ISIC/ISIC2020/jpeg/test_1024/'
 def file_name(file_dir):   
     L=[]   
     for root, dirs, files in os.walk(file_dir):  
@@ -10,10 +11,10 @@ def file_name(file_dir):
                 L.append(os.path.join(root, file))  
     return L  
 
-L=file_name('/home/data/ISIC/ISIC2020/jpeg/test')
+L=file_name(img_path)
 print(len(L))
 for path in L:
     pic = Image.open(path)
     pic = pic.resize((1024, 1024))
-    pic.save('/home/data/ISIC/ISIC2020/jpeg/test_1024/'+path.split('/')[-1])
+    pic.save(save_path+path.split('/')[-1])
 print('over')
